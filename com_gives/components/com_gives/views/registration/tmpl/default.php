73df7eb7a57fafae4d157eddb182d0278979f0b8
Jonathan Bennett <jbennett@ccistudios.com>
2011-05-03 13:58:57 -0400

    Removed registration view

--- com_gives/components/com_gives/views/registration/tmpl/default.php
+++
@@ -1,4 +0,0 @@
-<h1 class="componentheading"><?=@text('Register')?></h1>
-
-<p>Click <a href="<?=@route('view=volunteer&layout=form')?>">here</a> if you would like to register as a volunteer.</p>
-<p>Click <a href="<?=@route('view=organization&layout=form')?>">here</a> if you would like to register an organization.</p>
