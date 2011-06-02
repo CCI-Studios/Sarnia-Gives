f7ab13358837e7a1100b1e855e838c896bdca18d
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-02 15:15:14 -0400

    Added nofollow while in dev. Screw you Google, no more pre-production sites for you!

--- template/templates/sarnia_gives/index.php
+++ template/templates/sarnia_gives/index.php
@@ -32,6 +32,10 @@ $analytics = "UA-XXXXX-X"; // FIXME Update to client ID
 	<?= ($testing)? '':  '<meta http-equiv="X-UA-Compatible" contents="IE=edge,chrome=1">' ?>
 
  	<jdoc:include type="head" />
+ 	
+ 	<?php if ($testing): ?>
+ 	     <meta name="robots" content="noindex, nofollow" /> 
+    <?php endif; ?>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" href="/templates/<?= $this->template ?>/resources/favicon.ico">
