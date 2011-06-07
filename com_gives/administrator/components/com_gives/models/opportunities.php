2cfb3d8aaeaa419659273b67a6157807467f26ca
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-07 16:03:49 -0400

    Removed query filters

--- com_gives/administrator/components/com_gives/models/opportunities.php
+++ com_gives/administrator/components/com_gives/models/opportunities.php
@@ -6,21 +6,5 @@ class ComGivesModelOpportunities extends ComDefaultModelDefault
 	public function __construct(KConfig $config)
 	{
 		parent::__construct($config);
-		
-		$this->_state
-			->insert('detail', 'cmd');
-	}
-	
-	protected function _buildQueryColumns(KDatabaseQuery $query)
-	{
-		if (isset($this->_state->detail)) {
-			if ($this->_state->detail === 'short') {
-				$query->select(array(
-					'tbl.gives_opportunity_id',
-					'tbl.title',
-				));
-			}
-		}
-	}
-	
+	}	
 }
