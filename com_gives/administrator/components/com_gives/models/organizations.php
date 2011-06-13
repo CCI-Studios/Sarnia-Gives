65dbb17701f989265030e1cd156bedcc522da321
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-13 15:42:18 -0400

    Added filter by first letter option to organizations

--- com_gives/administrator/components/com_gives/models/organizations.php
+++ com_gives/administrator/components/com_gives/models/organizations.php
@@ -6,5 +6,37 @@ class ComGivesModelOrganizations extends ComDefaultModelDefault
 	public function __construct(KConfig $config)
 	{
 		parent::__construct($config);
+		
+		$this->_state
+			->insert('letter_name', 'word');
+	}
+	
+	public function getLetters() 
+	{
+		$database = KFactory::get($this->getTable())->getDatabase();
+		
+		$query = $database->getQuery()
+			->distinct()
+			->from('gives_organizations AS tbl')
+			->order('letter_name');
+		
+		$this->_buildQueryWhere($query);
+			
+		$query->columns[] = 'LEFT(tbl.title, 1) AS letter_name';
+
+
+		$result = $database->select($query, KDatabase::FETCH_FIELD_LIST);
+		return $result;
+	}
+	
+	protected function _buildQueryWhere(KDatabaseQuery $query)
+	{
+		$state = $this->_state;
+		
+		if ($state->letter_name) {
+			$query->where('tbl.title', 'LIKE', $state->letter_name.'%');
+		}	
+		
+		parent::_buildQueryWhere($query);
 	}
 }
--- com_gives/components/com_gives/views/organizations/html.php
+++ com_gives/components/com_gives/views/organizations/html.php
@@ -5,8 +5,7 @@ class ComGivesViewOrganizationsHtml extends ComDefaultViewHtml
 	
 	public function display()
 	{
-		$model = KFactory::tmp('admin::com.gives.model.organizations');
-		// $this->assign('letters_name', $model->getLetters()); /* fixme Get Letters */
+		$this->assign('letters', $this->getModel()->getLetters());
 		
 		return parent::display();
 	}
