f8e983c35145330e557e35cb420a1e5c2775568e
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-13 11:27:13 -0400

    Added validation to registration form

--- com_gives/components/com_gives/views/volunteer/tmpl/form.php
+++ com_gives/components/com_gives/views/volunteer/tmpl/form.php
@@ -1,4 +1,5 @@
-<? JHtml::_('behavior.mootools'); ?>
+<?= @helper('behavior.mootools') ?>
+<?= @helper('behavior.validator') ?>
 <script src="media://lib_koowa/js/koowa.js" />
 
 <h1 class="componentheading">
@@ -15,27 +16,27 @@
     
     <dl>
         <dt><label for="fname_field"><?= @text('First Name') ?>:</label></dt>
-        <dd><input type="text" name="first_name" id="fname_field" value="<?= $volunteer->first_name?>" /></dd>
+        <dd><input class="required" type="text" name="first_name" id="fname_field" value="<?= $volunteer->first_name?>" /></dd>
     </dl>
     
     <dl>
         <dt><label for="lname_field"><?= @text('Last Name') ?>:</label></dt>
-        <dd><input type="text" name="last_name" id="lname_field" value="<?= $volunteer->last_name ?>" /></dd>
+        <dd><input class="required" type="text" name="last_name" id="lname_field" value="<?= $volunteer->last_name ?>" /></dd>
     </dl>
     
     <dl>
         <dt><label for="email_field"><?= @text('Email Address') ?>:</label></dt>
-        <dd><input type="text" name="email" id="email_field" value="<?= $volunteer->email ?>" /></dd>
+        <dd><input class="required validate-email" type="text" name="email" id="email_field" value="<?= $volunteer->email ?>" /></dd>
     </dl>
     
     <dl>
         <dt><label for="password_field"><?= @text('Password') ?>:</label></dt>
-        <dd><input type="password" name="password" id="password_field" value="" /></dd>
+        <dd><input class="required" type="password" name="password" id="password_field" value="" /></dd>
     </dl>
     
     <dl>
         <dt><label for="confirmation_field"><?= @text('Password confirmation') ?>:</label></dt>
-        <dd><input type="password" name="confirmation" id="confirmation_field" value="" /></dd>
+        <dd><input class="required" type="password" name="confirmation" id="confirmation_field" value="" /></dd>
     </dl>
     
     <dl>
