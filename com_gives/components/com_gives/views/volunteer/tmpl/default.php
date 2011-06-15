6883fccd419ebb2534a5d306bdd779c58b6f1cfc
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-15 10:13:14 -0400

    Updated profile layout

--- com_gives/components/com_gives/views/volunteer/tmpl/default.php
+++ com_gives/components/com_gives/views/volunteer/tmpl/default.php
@@ -1,1 +1,23 @@
-<h1>my profile</h1>
+<h1><?= $title ?></h1>
+
+<div><div>
+	<p><?= $volunteer->firstname .' '. $volunteer->last_name ?></p>
+	
+	<? if ($edit_button): ?>
+		<p><a href="<?= @route('view=volunteer&layout=form&id='.$volunteer->id) ?>">
+			<?= @text('Edit Profile') ?>
+		</a></p>
+	<? endif; ?>
+</div></div>
+
+<div class="half"><div>
+	<h2>My Interests</h2>
+	
+	<p>adsf</p>
+</div></div>
+
+<div class="half"><div>
+	<h2>My Skills</h2>
+	
+	<p>adsf</p>
+</div></div>
