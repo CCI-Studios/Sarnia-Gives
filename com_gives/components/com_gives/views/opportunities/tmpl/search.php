915b9cbd9e03053a87fcb5b9ce5f7d5131bafb67
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-15 16:50:33 -0400

    Initial search page

---
+++ com_gives/components/com_gives/views/opportunities/tmpl/search.php
@@ -0,0 +1,28 @@
+<style src="media://com_gives/css/site.css" />
+
+<div class="com_gives">
+	<h1><?= @text('Search for Opportunities') ?></h1>
+
+	<p>Select your interests below to search for volunteer opportunities. Alternativly, you can seach for locations near you using the <a href="#">map</a>.</p>
+
+	<form action="<?= @route('') ?>" method="get">
+		<input type="hidden" name="layout" value="default">
+
+		<div class="box"><div>
+			<h2>Interests</h2>
+			<?= @helper('listbox.interests') ?>
+		</div></div>
+
+		<div class="box"><div>
+			<h2>Skills</h2>
+			<?= @helper('listbox.skills') ?>
+		</div></div>
+
+		<div class="box"><div>
+			<h2>Locations</h2>
+			<?= @helper('listbox.locations') ?>
+		</div></div>
+
+		<?= @helper('form.submit') ?>
+	</form>
+</div>
