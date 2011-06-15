61c13ca8c2c00ee0d65c3f5de26a8769b0bc5f33
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-15 17:00:36 -0400

    Filter opportunities based on location/skills/interests and possibly be fuzzy.

--- com_gives/administrator/components/com_gives/models/opportunities.php
+++ com_gives/administrator/components/com_gives/models/opportunities.php
@@ -6,5 +6,37 @@ class ComGivesModelOpportunities extends ComDefaultModelDefault
 	public function __construct(KConfig $config)
 	{
 		parent::__construct($config);
+		
+		$this->_state
+			->insert('locations', 'raw')
+			->insert('skills', 'raw')
+			->insert('interests', 'raw')
+			->insert('fuzzy', 'boolean', false);
 	}	
+	
+	
+	public function _buildQueryWhere(KDatabaseQuery $query)
+	{
+		$state = $this->_state;
+		
+		if ($state->fuzzy) {
+			$op = 'OR';
+		} else {
+			$op = 'AND';
+		}
+		
+		if (count($state->locations)) {
+			$query->where('tbl.locations', 'REGEXP', implode('|', $state->locations), $op);
+		}
+		
+		if (count($state->interests)) {
+			$query->where('tbl.interests', 'REGEXP', implode('|', $state->interests), $op);
+		}
+		
+		if (count($state->skills)) {
+			$query->where('tbl.skills', 'REGEXP', implode('|', $state->skills), $op);
+		}
+		
+		parent::_buildQueryWhere($query);
+	}
 }
