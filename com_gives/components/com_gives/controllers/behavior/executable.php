62400e705c3bac043e9f5dd6d2421ea48bcb8681
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-16 02:40:25 -0400

    Allow volunteers and organizations to save their info

--- com_gives/components/com_gives/controllers/behavior/executable.php
+++ com_gives/components/com_gives/controllers/behavior/executable.php
@@ -12,4 +12,18 @@ class ComGivesControllerBehaviorExecutable extends ComDefaultControllerBehaviorE
         return parent::_beforeAdd($context);
     }
 
+	protected function _beforeSave(KCommandContext $context)
+	{
+		$caller = $context->caller->getIdentifier()->name;
+
+		if ($caller == 'volunteer' || $caller == 'organization') {
+			$me = $this->getModel()->getMe();
+			$row = $this->getModel()->getItem();
+			
+			return $me->id == $row->user_id;
+		}
+		
+		return parent::_beforeSave($context);
+	}
+
 }
