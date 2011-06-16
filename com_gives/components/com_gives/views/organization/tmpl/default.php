934e4615c5ea7891c4455859681e73b667cedb81
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-16 02:54:42 -0400

    Changed contact info panel

--- com_gives/components/com_gives/views/organization/tmpl/default.php
+++ com_gives/components/com_gives/views/organization/tmpl/default.php
@@ -1,36 +1,41 @@
 <style src="media://com_gives/css/site.css" />
 
-<h1 class="componentheading">
-	<?= $organization->title ?>
+<div class="com_gives">
+	<h1 class="componentheading">
+		<?= $organization->title ?>
 
-	<? if ($organization->canEdit()): ?>
-		<div class="right">
-			<small><a href="<?= @route('view=organization&layout=edit&id='.$organization->id)?>">
-				<img src="/images/M_images/edit.png" />
-			</a></small>
+		<? if ($organization->canEdit()): ?>
+			<div class="right">
+				<small><a href="<?= @route('view=organization&layout=edit&id='.$organization->id)?>">
+					<img src="/images/M_images/edit.png" />
+				</a></small>
+			</div>
+		<? endif; ?>
+	</h1>
+
+	<div>
+		<div class="contact_info">
+			<h2><?= @text('Contact Info')?></h2>
+		
+			<div class="column">
+				<?=$organization->address?><br/>
+				<?=$organization->city?><br/>
+				<?=$organization->province?><br/>
+			</div>
+			<div class="column">
+				<?=$organization->phone?><br/>
+				<?=$organization->fax?><br/>
+				<a href="mailto:<?=$organization->email?>"><?=$organization->email?></a><br/>
+			</div>
 		</div>
-	<? endif; ?>
-</h1>
-
-<div>
-	<h2><?= @text('Organization Details')?></h2>
 	
-	<div class="column-group">
-		<div class="column">
-			<?=$organization->address?><br/>
-			<?=$organization->city?><br/>
-			<?=$organization->province?><br/>
-		</div>
-		<div class="column">
-			<?=$organization->phone?><br/>
-			<?=$organization->fax?><br/>
-			<a href="mailto:<?=$organization->email?>"><?=$organization->email?></a><br/>
-		</div>
+		<h2><?= @text('Our Mission')?></h2>
+		<p><?=$organization->mission?></p>
+	
+		<h2><?=@text('Description')?></h2>
+		<p><?=$organization->description?></p>
 	</div>
 	
-	<h2><?= @text('Our Mission')?></h2>
-	<p><?=$organization->mission?></p>
-	
-	<h2><?=@text('Description')?></h2>
-	<p><?=$organization->description?></p>
+	<div>
+		<p>ADD OPPORTUNITIES</p>
 </div>
