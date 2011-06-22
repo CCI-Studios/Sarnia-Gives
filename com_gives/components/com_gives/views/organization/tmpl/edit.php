5ee3cad7f606d2edfbb1b3253761f467036983e6
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-22 11:08:24 -0400

    Added more editing fields for organizations

--- com_gives/components/com_gives/views/organization/tmpl/edit.php
+++ com_gives/components/com_gives/views/organization/tmpl/edit.php
@@ -8,6 +8,47 @@
 	<form action="<?= @route('id='.$organization->id)?>" method="post">
 		<input type="hidden" name="action" value="save" />
 	
+		<fieldset>
+			<legend><?= @text('Contact Information') ?></legend>
+			
+			<?= @helper('form.text', array('name'=>'address')) ?>
+			<?= @helper('form.text', array('name'=>'city')) ?>
+			<dl>
+				<dt><label for=""><?= @text('Province') ?>:</label></dt>
+				<dd><?= @helper('site::com.gives.template.helper.listbox.provinces', array()) ?></dd>
+			</dl>
+			<?= @helper('form.text', array('name'=>'postal')) ?>
+			<?= @helper('form.text', array('name'=>'phone')) ?>
+			<?= @helper('form.text', array('name'=>'fax')) ?>
+			<?= @helper('form.text', array('name'=>'email')) ?>
+			<?= @helper('form.text', array('name'=>'website')) ?>
+		</fieldset>
+		
+		<fieldset>
+			<legend>Organization Details</legend>
+			
+			<?= @helper('form.text', array('name'=>'director')) ?>
+			<?= @helper('form.text', array('name'=>'contact')) ?>
+			<?= @helper('form.text', array('name'=>'contact_email')) ?>
+			<?= @helper('form.text', array('name'=>'contact_title')) ?>
+			<dl>
+				<dt><label for=""><?= @text('Organization Type') ?>:</label></dt>
+				<dd><?= @helper('site::com.gives.template.helper.listbox.organizationTypes', array()) ?></dd>
+			</dl>
+		</fieldset>
+
+<? if (false): ?>		
+		<fieldset>
+			<legend>Organization Mission</legend>
+			<?= @helper('admin::com.default.template.helper.editor.display', array('name'=>'mission', 'height'=>150, 'buttons'=>false)) ?>
+		</fieldset>
+		
+		<fieldset>
+			<legend>Organization Description</legend>
+			<?= @helper('admin::com.default.template.helper.editor.display', array('height'=>150, 'buttons'=>false)) ?>
+		</fieldset>
+<? endif; ?>
+	
 		<h2>Your Interests</h2>
 		<?= @helper('listbox.interests')?>
 		
