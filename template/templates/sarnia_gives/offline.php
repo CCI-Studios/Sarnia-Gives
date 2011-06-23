22f6c6b6fcb63a751960b73de8c2806d8e2df898
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-23 14:40:49 -0400

    Added offline page

---
+++ template/templates/sarnia_gives/offline.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<title>Sarnia Gives | Coming Soon</title> 
+ 
+	<style type="text/css"> 
+	body {
+		font-family: helvetica, arial, sans-serif;
+		margin: 0;
+	}
+	#wrapper {
+		margin: 50px auto 0;
+		text-align: center;
+		width: 960px;
+	}
+	</style> 
+</head> 
+ 
+<body> 
+	<div id="wrapper"> 
+		<p><img src="/templates/sarnia_gives/images/logo.jpeg" width="479" height="200" /></p> 
+		<p><?= JFactory::getApplication()->getCfg('offline_message'); ?></p> 
+	</div> 
+</body> 
+</html>
