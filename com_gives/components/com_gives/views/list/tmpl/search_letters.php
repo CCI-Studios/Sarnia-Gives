37404569db5934697a53b69053db2813750d517e
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-07 11:59:47 -0400

    Use search letters template

--- com_gives/components/com_gives/views/list/tmpl/search_letters.php
+++ com_gives/components/com_gives/views/list/tmpl/search_letters.php
@@ -1,18 +1,16 @@
 <? defined('KOOWA') or die; ?>
 
 <ul class="search_letters">
-	<? foreach ($letters_name as $letter): ?>
+	<? foreach ($letters as $letter): ?>
 		<? $class = ($state->letter_name == $letter)? 'class="active"':'' ?>
-		<li <?= $class ?>>
-			<a href="<?= @route('letter_name='.$letter) ?>">
-				<?= $letter; ?>
-			</a>
+		<li>
+			<a <?= $class ?> href="<?= @route('letter_name='.$letter) ?>"><?= $letter; ?></a>
 		</li>
 	<? endforeach; ?>
 	
+	<? if (isset($state->letter_name)): ?>
 	<li>
-		<a href="<?= @route('letter_name=') ?>">
-			<?= @text('All')?>
-		</a>
+		<a href="<?= @route('letter_name=') ?>"><?= @text('All')?></a>
 	</li>
+	<? endif; ?>
 </ul>
--- com_gives/components/com_gives/views/organizations/tmpl/default.php
+++ com_gives/components/com_gives/views/organizations/tmpl/default.php
@@ -4,21 +4,8 @@
 
 <h1 class="componentheading">Organizations</h1>
 
-<ul class="search_letters">	
-	<? foreach($letters as $letter) : ?>
-	<? $class = ($state->letter_name == $letter) ? 'class="active" ' : ''; ?>
-	<li>
-		<a href="<?= @route('letter_name='.$letter) ?>" <?= $class ?>>
-			<?= $letter; ?>
-		</a>
-	</li>
-	<? endforeach; ?>
-	<li>
-		<a href="<?= @route('letter_name=') ?>">
-			<?= @text('Reset'); ?>
-		</a>
-	</li>
-</ul>
+<?= @template('site::com.gives.views.list.search_letters', 
+	array('letters' => $letters)); ?>
 
 <ul class="organization">
 <? foreach ($organizations as $organization): ?>
