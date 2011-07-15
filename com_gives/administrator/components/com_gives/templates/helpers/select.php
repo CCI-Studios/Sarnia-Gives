1aff34dca7550168225407ae29fb6bd004fc5473
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-15 12:29:38 -0400

    Fixed checklist settings

--- com_gives/administrator/components/com_gives/templates/helpers/select.php
+++ com_gives/administrator/components/com_gives/templates/helpers/select.php
@@ -7,31 +7,27 @@ class ComGivesTemplateHelperSelect extends KTemplateHelperSelect
 		$config = new KConfig($config);
 		$config->append(array(
 			'name'		=> 'interests',
-			'attribs' 	=> array(
-				'multiple'	=>'multiple',
-				'style'		=>'width: 100%;',
-				'size'		=> 10,
-			),
+			'text'		=> 'text',
+			'key'		=> 'value',
 		))->append(array(
 			'selected'	=> $config->{$config->name},
 		));
-		$config->name = $config->name.'[]';
 		
 		$options = array();
 		
-		$options[] = $this->option(array('text'=>"Animals/Animal Services", 'value'=>"Animals"));
-		$options[] = $this->option(array('text'=>"Community Development", 'value'=>"Community Development"));
-		$options[] = $this->option(array('text'=>"Health &amp; Wellness", 'value'=>"Health & Wellness"));
-		$options[] = $this->option(array('text'=>"Arts &amp; Culture", 'value'=>"Arts & Culture"));
-		$options[] = $this->option(array('text'=>"Emergency/Crisis Services", 'value'=>"Emergency/Crisis Services"));
-		$options[] = $this->option(array('text'=>"Social Services &amp; Justice", 'value'=>"Social Services & Justice"));
-		$options[] = $this->option(array('text'=>"Children &amp; Youth", 'value'=>"Children & Youth"));
-		$options[] = $this->option(array('text'=>"Environment", 'value'=>"Environment"));
-		$options[] = $this->option(array('text'=>"Special Events", 'value'=>"Special Events"));
+		$options[] = new KConfig(array('text'=>"Animals/Animal Services", 'value'=>"Animals"));
+		$options[] = new KConfig(array('text'=>"Community Development", 'value'=>"Community Development"));
+		$options[] = new KConfig(array('text'=>"Health &amp; Wellness", 'value'=>"Health & Wellness"));
+		$options[] = new KConfig(array('text'=>"Arts &amp; Culture", 'value'=>"Arts & Culture"));
+		$options[] = new KConfig(array('text'=>"Emergency/Crisis Services", 'value'=>"Emergency/Crisis Services"));
+		$options[] = new KConfig(array('text'=>"Social Services &amp; Justice", 'value'=>"Social Services & Justice"));
+		$options[] = new KConfig(array('text'=>"Children &amp; Youth", 'value'=>"Children & Youth"));
+		$options[] = new KConfig(array('text'=>"Environment", 'value'=>"Environment"));
+		$options[] = new KConfig(array('text'=>"Special Events", 'value'=>"Special Events"));
 		
-		$config->options = $options;
+		$config->list = $options;
 		
-		return $this->optionlist($config);
+		return $this->checklist($config);
 	}
 	
 	public function locations($config = array())
@@ -39,33 +35,29 @@ class ComGivesTemplateHelperSelect extends KTemplateHelperSelect
 		$config = new KConfig($config);
 		$config->append(array(
 			'name'		=> 'locations',
-			'attribs' 	=> array(
-				'multiple'=>'multiple',
-				'style'=>'width: 100%;',
-				'size'		=> 10,
-			),
+			'text'		=> 'text',
+			'key'		=> 'value',
 		))->append(array(
 			'selected'	=> $config->{$config->name},
 		));
-		$config->name = $config->name.'[]';
 		
 		$options = array();
 		
-		$options[] = $this->option(array('text'=>"Brooke-Alvinston", 'value'=>"Brooke-Alvinston"));
-		$options[] = $this->option(array('text'=>"Lambton Shores", 'value'=>"Lambton Shores"));
-		$options[] = $this->option(array('text'=>"Point Edward", 'value'=>"Point Edward"));
-		$options[] = $this->option(array('text'=>"Sarnia", 'value'=>"Sarnia"));
-		$options[] = $this->option(array('text'=>"Dawn-Euphemia", 'value'=>"Dawn-Euphemia"));
-		$options[] = $this->option(array('text'=>"Oil Springs", 'value'=>"Oil Springs"));
-		$options[] = $this->option(array('text'=>"Plympton Wyoming", 'value'=>"Plympton Wyoming"));
-		$options[] = $this->option(array('text'=>"Enniskillen", 'value'=>"Enniskillen"));
-		$options[] = $this->option(array('text'=>"Petrolia", 'value'=>"Petrolia"));
-		$options[] = $this->option(array('text'=>"St. Clair", 'value'=>"St. Clair"));
-		$options[] = $this->option(array('text'=>"Location Flexible", 'value'=>"Location Flexible"));
+		$options[] = new KConfig(array('text'=>"Brooke-Alvinston", 'value'=>"Brooke-Alvinston"));
+		$options[] = new KConfig(array('text'=>"Lambton Shores", 'value'=>"Lambton Shores"));
+		$options[] = new KConfig(array('text'=>"Point Edward", 'value'=>"Point Edward"));
+		$options[] = new KConfig(array('text'=>"Sarnia", 'value'=>"Sarnia"));
+		$options[] = new KConfig(array('text'=>"Dawn-Euphemia", 'value'=>"Dawn-Euphemia"));
+		$options[] = new KConfig(array('text'=>"Oil Springs", 'value'=>"Oil Springs"));
+		$options[] = new KConfig(array('text'=>"Plympton Wyoming", 'value'=>"Plympton Wyoming"));
+		$options[] = new KConfig(array('text'=>"Enniskillen", 'value'=>"Enniskillen"));
+		$options[] = new KConfig(array('text'=>"Petrolia", 'value'=>"Petrolia"));
+		$options[] = new KConfig(array('text'=>"St. Clair", 'value'=>"St. Clair"));
+		$options[] = new KConfig(array('text'=>"Location Flexible", 'value'=>"Location Flexible"));
 		
-		$config->options = $options;
+		$config->list = $options;
 		
-		return $this->optionlist($config);
+		return $this->checklist($config);
 	}
 	
 	public function skills($config = array())
@@ -73,41 +65,84 @@ class ComGivesTemplateHelperSelect extends KTemplateHelperSelect
 		$config = new KConfig($config);
 		$config->append(array(
 			'name'		=> 'skills',
-			'attribs' 	=> array(
-				'multiple'	=>'multiple',
-				'style'		=>'width: 100%;',
-				'size'		=> 10,
-			),
+			'text'		=> 'text',
+			'key'		=> 'value'
 		))->append(array(
 			'selected'	=> $config->{$config->name},
 		));
-		$config->name = $config->name.'[]';
 		
 		$options = array();
 		
-		$options[] = $this->option(array('text'=>"Accounting", 'value'=>"Accounting"));
-		$options[] = $this->option(array('text'=>"Coaching", 'value'=>"Coaching"));
-		$options[] = $this->option(array('text'=>"Event Coordination", 'value'=>"Event Coordination"));
-		$options[] = $this->option(array('text'=>"IT", 'value'=>"IT"));
-		$options[] = $this->option(array('text'=>"Marketing & Communications", 'value'=>"Marketing & Communications"));
-		$options[] = $this->option(array('text'=>"Outreach", 'value'=>"Outreach"));
-		$options[] = $this->option(array('text'=>"Translation", 'value'=>"Translation"));
-		$options[] = $this->option(array('text'=>"Bilingual (Eng/Fr)", 'value'=>"Bilingual (Eng/Fr)"));
-		$options[] = $this->option(array('text'=>"Counseling", 'value'=>"Counseling"));
-		$options[] = $this->option(array('text'=>"Fundraising", 'value'=>"Fundraising"));
-		$options[] = $this->option(array('text'=>"Legal", 'value'=>"Legal"));
-		$options[] = $this->option(array('text'=>"Medical Assistance", 'value'=>"Medical Assistance"));
-		$options[] = $this->option(array('text'=>"PR", 'value'=>"PR"));
-		$options[] = $this->option(array('text'=>"Writing & Research", 'value'=>"Writing & Research"));
-		$options[] = $this->option(array('text'=>"Board Work", 'value'=>"Board Work"));
-		$options[] = $this->option(array('text'=>"", 'value'=>"General Administration"));
-		$options[] = $this->option(array('text'=>"Management Consulting", 'value'=>"Management Consulting"));
-		$options[] = $this->option(array('text'=>"Mentoring & Training", 'value'=>"Mentoring & Training"));
-		$options[] = $this->option(array('text'=>"Sports & Recreation", 'value'=>"Sports & Recreation"));
-		$options[] = $this->option(array('text'=>"", 'value'=>""));
+		$options[] = new KConfig(array('text'=>"Accounting", 'value'=>"Accounting"));
+		$options[] = new KConfig(array('text'=>"Coaching", 'value'=>"Coaching"));
+		$options[] = new KConfig(array('text'=>"Event Coordination", 'value'=>"Event Coordination"));
+		$options[] = new KConfig(array('text'=>"IT", 'value'=>"IT"));
+		$options[] = new KConfig(array('text'=>"Marketing & Communications", 'value'=>"Marketing & Communications"));
+		$options[] = new KConfig(array('text'=>"Outreach", 'value'=>"Outreach"));
+		$options[] = new KConfig(array('text'=>"Translation", 'value'=>"Translation"));
+		$options[] = new KConfig(array('text'=>"Bilingual (Eng/Fr)", 'value'=>"Bilingual (Eng/Fr)"));
+		$options[] = new KConfig(array('text'=>"Counseling", 'value'=>"Counseling"));
+		$options[] = new KConfig(array('text'=>"Fundraising", 'value'=>"Fundraising"));
+		$options[] = new KConfig(array('text'=>"Legal", 'value'=>"Legal"));
+		$options[] = new KConfig(array('text'=>"Medical Assistance", 'value'=>"Medical Assistance"));
+		$options[] = new KConfig(array('text'=>"PR", 'value'=>"PR"));
+		$options[] = new KConfig(array('text'=>"Writing & Research", 'value'=>"Writing & Research"));
+		$options[] = new KConfig(array('text'=>"Board Work", 'value'=>"Board Work"));
+		$options[] = new KConfig(array('text'=>"General Administration", 'value'=>"General Administration"));
+		$options[] = new KConfig(array('text'=>"Management Consulting", 'value'=>"Management Consulting"));
+		$options[] = new KConfig(array('text'=>"Mentoring & Training", 'value'=>"Mentoring & Training"));
+		$options[] = new KConfig(array('text'=>"Sports & Recreation", 'value'=>"Sports & Recreation"));
 		
-		$config->options = $options;
+		$config->list = $options;
 		
-		return $this->optionlist($config);
+		return $this->checklist($config);
 	}
+	
+	public function checklist( $config = array())
+	{
+		$config = new KConfig($config);
+		$config->append(array(
+			'list' 		=> null,
+			'name'   	=> 'id',
+			'attribs'	=> array(),
+			'key'		=> 'id',
+			'text'		=> 'title',
+			'selected'	=> null,
+			'translate'	=> false
+		));
+
+		$name    = $config->name;
+		$attribs = KHelperArray::toString($config->attribs);
+
+		$html = array();
+		foreach($config->list as $row)
+		{
+			$key  = $row->{$config->key};
+			$text = $config->translate ? JText::_( $row->{$config->text} ) : $row->{$config->text};
+			$id	  = isset($row->id) ? $row->id : null;
+
+			$extra = '';
+
+			if ($config->selected instanceof KConfig)
+			{
+				foreach ($config->selected as $value)
+				{
+					$sel = is_object( $value ) ? $value->{$config->key} : $value;
+					if ($key == $sel)
+					{
+						$extra .= 'checked="checked"';
+						break;
+					}
+				}
+			} 
+			else $extra .= ($key == $config->selected) ? 'checked="checked"' : '';
+
+			$html[] = '<input type="checkbox" name="'.$name.'[]" id="'.$name.$key.'" value="'.$key.'" '.$extra.' '.$attribs.' />';
+			$html[] = '<label for="'.$name.$key.'">'.$text.'</label>';
+			$html[] = '<br/>';
+		}
+
+		return implode(PHP_EOL, $html);
+	}
+	
 }
