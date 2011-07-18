e60af4ebf196dbf219db60cf5c696d238487af03
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-18 14:48:44 -0400

    Changed inline styles to classes

--- com_gives/components/com_gives/views/opportunities/tmpl/search_results.php
+++ com_gives/components/com_gives/views/opportunities/tmpl/search_results.php
@@ -1,15 +1,15 @@
 <? defined('KOOWA') or die; ?>
 
-<ul style="border: 1px solid black; padding: 5px; background: ; list-style: none;">
+<ul class="opportunity_list">
 	<? foreach ($opportunities as $opportunity): ?>
 	<? $org = KFactory::tmp('admin::com.gives.model.organization')
 		->set('id', $opportunity->gives_organization_id)
 		->getItem();
 	?>
-	<li style="border: 1px solid black; padding: 0 0 5px 2em;">
+	<li>
 		<? if ($org->logo): ?>
-		<div class="logo" style="float: left; margin: 5px 5px 5px 0;">
 			<img src="media://com_gives/uploads/organizations/small_<?= $org->logo ?>" alt="" />
+		<div class="logo">
 		</div>
 		<? endif; ?>
 		
@@ -18,12 +18,12 @@
 			<span style="font-weight: 500;"><?= $opportunity->title; ?></span>
 		</div>
 		
-		<div class="" style="float: right;">
-			<a style="display: block; padding: 0 5px; background-color: #77cc77;" href="<?= @route('view=opportunity&id='. $opportunity->id) ?>">
-				View More Details
+		<div class="details">
+			<a href="<?= @route('view=opportunity&id='. $opportunity->id) ?>">
+				View Opportunity
 			</a>
 		</div>
-
+		
 		<div class="clear"></div>
 	</li>
 	<? endforeach; ?>
