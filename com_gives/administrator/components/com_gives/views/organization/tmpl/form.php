b16e76382032bca61a9b2e7ef2b82833dd3700b3
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-24 23:15:36 -0400

    Added js validation

--- com_gives/administrator/components/com_gives/views/organization/tmpl/form.php
+++ com_gives/administrator/components/com_gives/views/organization/tmpl/form.php
@@ -1,4 +1,5 @@
 <? defined('KOOWA') or die; ?>
+<?= @helper('behavior.validator') ?>
 <script src="media://lib_koowa/js/koowa.js" />
 <style src="media://com_default/css/form.css" />
 <style src="media://com_gives/css/admin.css" />
@@ -11,7 +12,7 @@
 			<legend><?= @text('Organization Details') ?></legend>
 			
 			<label for="field_title" class="mainlabel"><?=@text('Name')?>:</label>
-			<input type="text" name="title" id="field_title" value="<?=$organization->title?>" /><br/>
+			<input type="text" class="required" name="title" id="field_title" value="<?=$organization->title?>" /><br/>
 			
 			<label for="field_address" class="mainlabel"><?=@text('Address')?>:</label>
 			<input type="text" name="address" id="field_address" value="<?=$organization->address?>" /><br/>
@@ -40,11 +41,11 @@
 			<label for="field_director" class="mainlabel"><?=@text('Director')?>:</label>
 			<input type="text" name="director" id="field_director" value="<?=$organization->director?>" /><br/>
 			
-			<label for="field_contact" class="mainlabel"><?=@text('Contact')?>:</label>
-			<input type="text" name="contact" id="field_contact" value="<?=$organization->contact?>" /><br/>
+			<label for="field_contact" class="mainlabel"><?=@text('Contact Name')?>:</label>
+			<input type="text" class="required" name="contact" id="field_contact" value="<?=$organization->contact?>" /><br/>
 			
 			<label for="field_contact_email" class="mainlabel"><?=@text('Contact Email')?>:</label>
-			<input type="text" name="contact_email" id="field_contact_email" value="<?=$organization->contact_email?>" /><br/>
+			<input type="text" class="required" name="contact_email" id="field_contact_email" value="<?=$organization->contact_email?>" /><br/>
 			
 			<label for="field_contact_title" class="mainlabel"><?=@text('Contact Title')?>:</label>
 			<input type="text" name="contact_title" id="field_contact_title" value="<?=$organization->contact_title?>" /><br/>
