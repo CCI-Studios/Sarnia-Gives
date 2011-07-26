01ff1d3d3768fd71f993f53f1e0c602d1c3afec6
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-26 14:57:26 -0400

    Added distance/location code to model

--- com_gives/administrator/components/com_gives/models/opportunities.php
+++ com_gives/administrator/components/com_gives/models/opportunities.php
@@ -14,9 +14,21 @@ class ComGivesModelOpportunities extends ComDefaultModelDefault
 			->insert('interests', 'raw')
 			->insert('enabled', 'boolean', false)
 			->insert('fuzzy', 'boolean', false)
+			->insert('address', 'string')
+			->insert('distance', 'float')
 			->remove('sort')
 			->insert('sort', 'cmd', 'title'); // FIXME group by organization
 	}	
+	
+	public function _buildQueryColumns(KDatabaseQuery $query)
+	{
+		if (is_numeric($this->_state->distance) && isset($this->_state->address)) {
+			list($lat, $lng) = $this->_getLocation($this->_state->address);
+			$query->select("(6371 * ACOS(SIN(RADIANS($lat))*SIN(RADIANS(tbl.lat)) + COS(RADIANS($lat))*COS(RADIANS(tbl.lat))*COS(RADIANS($lng)-RADIANS(tbl.lng)))) AS distance");
+		}
+		
+		parent::_buildQueryColumns($query);
+	}
 	
 	
 	public function _buildQueryWhere(KDatabaseQuery $query)
@@ -49,6 +61,31 @@ class ComGivesModelOpportunities extends ComDefaultModelDefault
 			$query->where('tbl.skills', 'REGEXP', implode('|', $state->skills), $op);
 		}
 		
+		if (isset($state->address)) {
+			list($lat, $lng) = $this->_getLocation($state->address);
+			$query->where('tbl.lat', '!=', 'null');
+			$query->where('tbl.lng', '!=', 'null');
+		}
+		
 		parent::_buildQueryWhere($query);
+	}
+	
+	protected function _getLocation($address)
+	{
+		$ch = curl_init();
+		$url = 'http://maps.googleapis.com/maps/api/geocode/json?';
+		$data = array('sensor=false','address='.$address);
+		$url = $url . str_replace(' ', '+', implode('&', $data));
+		
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$results = json_decode(curl_exec($ch));
+		
+		if (isset($results->status) && $results->status === 'REQUEST_DENIED') {
+			return false;
+		} else {
+			$data = $results->results[0]->geometry->location;
+			return array($data->lat, $data->lng);
+		}
 	}
 }
