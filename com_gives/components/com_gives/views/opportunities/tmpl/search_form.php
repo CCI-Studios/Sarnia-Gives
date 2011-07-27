d38373d97bef828d383b22b4c7ef4105d1c1287f
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-27 07:04:04 -0400

    Added JS to block form submission if form isn't complete

--- com_gives/components/com_gives/views/opportunities/tmpl/search_form.php
+++ com_gives/components/com_gives/views/opportunities/tmpl/search_form.php
@@ -1,6 +1,45 @@
 <? defined('KOOWA') or die; ?>
 
-<form action="<?= @route('') ?>" method="get">
+<script>
+window.addEvent('domready', function () {
+	var form = $('criteria-search-form');
+
+	form.addEvent('submit', function (event) {
+		var interests = false, 
+			skills = false, 
+			locations = false;
+
+		form.getElements('input[name="interests[]"]').each(function (box) {
+			if (box.checked) {
+				console.log(box.value + ' was checked');
+				interests = true;
+			}
+		});
+
+		form.getElements('input[name="skills[]"]').each(function (box) {
+			if (box.checked) {
+				console.log(box.value + ' was checked');
+				skills = true;
+			}
+		});
+
+		form.getElements('input[name="locations[]"]').each(function (box) {
+			if (box.checked) {
+				console.log(box.value + ' was checked');
+				locations = true;
+			}
+		});
+
+		if (!(interests && skills && locations)) {
+			alert('Please make a selection for all sections.');
+		}
+
+		return (interests && skills && locations);
+	});
+});
+</script>
+
+<form action="<?= @route('') ?>" method="get" id="criteria-search-form">
 	<input type="hidden" name="layout" value="default">
 
 	<div><div>
