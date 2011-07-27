baf50f15c08a279bc17d46f4aa1e5474b5dbcb46
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-27 07:40:12 -0400

    Fixed model name

--- com_gives/components/com_gives/controllers/behavior/executable.php
+++ com_gives/components/com_gives/controllers/behavior/executable.php
@@ -27,7 +27,7 @@ class ComGivesControllerBehaviorExecutable extends ComDefaultControllerBehaviorE
 			$row = $this->getModel()->getItem();
 			
 			if ($caller == 'opportunity') {
-				$model = KFactory::get('site::com.gives.model.organization');
+				$model = KFactory::get('site::com.gives.model.organizations');
 				$org = $model->set('id', $row->gives_organization_id)->getItem();
 				
 				return $me->id === $org->user_id;
