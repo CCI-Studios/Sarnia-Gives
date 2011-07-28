a7c9c33613893f7f783b957032408ec77591a669
Jonathan Bennett <jbennett@ccistudios.com>
2011-07-27 20:07:12 -0400

    Redid default opportunity page

--- com_gives/components/com_gives/views/opportunity/tmpl/default.php
+++ com_gives/components/com_gives/views/opportunity/tmpl/default.php
@@ -2,7 +2,7 @@
 
 <div class="com_gives">
 	<h1>
-		<?= $opportunity->title ?>
+		<?= $organization->title .' - '. $opportunity->title ?>
 		
 		<? if ($edit): ?>
 		<span class="edit">
@@ -13,59 +13,20 @@
 		<? endif; ?>
 	</h1>
 	
-	<div class="contact_info">
-		<h2><?= @text('Contact Info for ') .' '. $organization->title ?></h2>
+	<h2>Description</h2>
+	<p><?= $opportunity->description ?></p>
 	
-		<div class="column">
-			<?=$organization->address?><br/>
-			<?=$organization->city?>, <?=$organization->province?><br/>
-			<?= ($organization->postal)? $organization->postal .'<br/>': ''?>
-			<br/>
-			<?= ($organization->phone)? @text('Phone') .': '. $organization->phone.'<br/>':'' ?>
-			<?= ($organization->fax)? @text('Fax') .': '. $organization->fax.'<br/>':'' ?>
-			<a href="mailto:<?=$organization->email?>"><?=$organization->email?></a><br/>
-		</div>
-		
-		<p>View <a href="<?= @route('view=organization&id='. $organization->id) ?>">
-			<?= $organization->title ?></a></p>
-	</div>
-	
-	<h2>Details</h2>
-	Start Date: <?= $opportunity->start_date ?><br/>
-	End Date: <?= $opportunity->end_date ?><br/>
+	<h2>Requirements</h2>
+	<p>police check<Br/>
+	license</p>
 	
 	<h2>Location</h2>
-	Address: <?= $opportunity->address ?><br/>
+	<p>Address: <?= $opportunity->address ?><br/>
 	City: <?= $opportunity->city ?><br/>
 	Province: <?= $opportunity->province ?><br/>
-	Postal: <?= $opportunity->postal ?><br/>
+	Postal: <?= $opportunity->postal ?></p>
 	
-	<? if (count($opportunity->interests)): ?>
-		<h2>Interests</h2>
-		<ul>
-			<? foreach ($opportunity->interests as $interest):?>
-			<li><?= $interest ?></li>
-			<? endforeach; ?>
-		</ul>
-	<? endif; ?>
-	
-	<? if (count($opportunity->skills)): ?>
-		<h2>Skills</h2>
-		<ul>
-			<? foreach ($opportunity->skills as $skill): ?>
-			<li><?= $skill ?></li>
-			<? endforeach; ?>
-		</ul>
-	<? endif; ?>
-	
-	<? if (count($opportunity->locations)): ?>
-		<h2>Location</h2>
-		<ul>
-			<? foreach ($opportunity->locations as $location): ?>
-			<li><?= $location ?></li>
-			<? endforeach; ?>
-		</ul>
-	<? endif; ?>
-	
-	<p>FIXME Clean up the styling of this page.</p>
+	<h2>Contact</h2>
+	<p>Call us at <?= $organization->phone ?> or email <a href="mailto:<?= $organization->email ?>"><?= $organization->contact?></a> for more information.</p>
+	<p style="margin-top:2em;">Click <a href="<?= @route('view=organization&id='. $organization->id) ?>">here</a> to view more opportunities from <a href="<?= @route('view=organization&id='. $organization->id) ?>"><?= $organization->title ?></a>.</p>
 </div>
