6af0585540a7edcf01e6eec3c0f570303358f0b3
Jonathan Bennett <jbennett@ccistudios.com>
2011-08-03 16:40:51 -0400

    Added notifications to opportunities

---
+++ com_gives/administrator/components/com_gives/databases/tables/opportunities.php
@@ -0,0 +1,53 @@
+<?php
+
+class ComGivesDatabaseTableOpportunities extends KDatabaseTableAbstract
+{
+	
+	public function insert(KDatabaseRowInterface $row)
+	{
+		$affected = parent::insert($row);
+		
+		if ($affected) {
+			$data = $row->getData();
+
+			$params = &JComponentHelper::getParams('com_gives');
+			if ($params->get('send_emails')) {
+				$this->_emailAdmin($data['title']);
+			}
+		}
+		
+		return $affected;
+	}
+	
+	protected function _emailAdmin($title)
+	{
+		$params = &JComponentHelper::getParams('com_gives');
+		$admins = $params->get('email_list');
+		if ($admins === '')
+			return;
+		
+		$view = KFactory::tmp("admin::com.default.template.default");
+		$view->loadIdentifier('admin::com.gives.view.opportunity.email_admin', array(
+			'name'	=> $name
+		));
+		
+		$this->_sendMail($admins, "New Sarnia Gives Opportunity", $view->render());
+	}
+	
+	protected function _sendMail($to, $subject, $body)
+	{
+		$mailer		= JFactory::getMailer();
+		$config		= JFactory::getConfig();
+		
+		$mailer->setSender(array(
+			$config->getValue('config.mailfrom'),
+			$config->getValue('config.fromname')
+		));
+		
+		$mailer->addRecipient($to);
+		$mailer->setSubject($subject);
+		$mailer->setBody($body);
+		$mailer->isHtml(true);
+		$mailer->send();
+	}
+}
---
+++ com_gives/administrator/components/com_gives/views/opportunity/tmpl/email_admin.php
@@ -0,0 +1,23 @@
+<? defined('KOOWA') or die; ?>
+
+<center>
+	<div style="width: 650px; border-bottom: 1px solid #c8c9cb; padding: 0 0 5px;">
+		<table width="100%" cellpadding="0" border="0" cellspacing="0">
+			<tr>
+				<td>
+					<img src="http://dev.sarniagives.com/images/stories/default/logo.png" alt="Sarnia Gives" />
+				</td>
+				
+				<td align="right" style="vertical-align: bottom;" valign="bottom">
+					<a href="mailto:info@sarniagives.com" style="color: #bbb;">info@sarniagives.com</a>
+					<a href="http://www.sarniagives.com" style="color: #bbb;">www.sarniagives.com</a>
+				</td>
+			</tr>
+		</table>
+	</div>
+	
+	<div style="width: 650px; text-align: left; ">
+		<p>A new opportunity, <?= $title; ?>, has been added to Sarnia Gives.</p>
+	</div>
+</center>
+		
