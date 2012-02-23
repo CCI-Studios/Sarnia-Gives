e395ca673e77b5ccbfc29c09f3688e8fc1526634
Jonathan Bennett <jbennett@ccistudios.com>
2012-02-23 12:24:37 -0500

    Added validation to checkboxes

--- com_gives/components/com_gives/views/organization/tmpl/form.php
+++ com_gives/components/com_gives/views/organization/tmpl/form.php
@@ -1,6 +1,19 @@
 <?= @helper('behavior.mootools') ?>
 <?= @helper('behavior.validator') ?>
 <script src="media://lib_koowa/js/koowa.js" />
+<script>
+Form.Validator.addAllThese([
+    ['validate-required-check', {
+        errorMsg: function(element, props){
+            return props.useTitle ? element.get('title') : Form.Validator.getMsg('requiredChk');
+        },
+        test: function(element, props){
+            return !!element.checked;
+        }
+    }]
+]);
+</script>
+
 
 <h1 class="componentheading">
     <?= @text('Organization Registration'); ?>
@@ -39,6 +52,27 @@
     <div>
     	<div class="label"><label for="confirmation_field"><?= @text('Password Confirmation') ?>:</label></div>
     	<div class="input"><input class="required" type="password" name="confirmation" id="confirmation_field" value="" /></div>
+    </div>
+
+    <div>
+        I hereby authorize verification of all statements herein and release Sarnia Gives and all others from liability in connection with the same.<br/>
+        I give Sarnia Gives permission to share the information provided with potential volunteers for the purpose of volunteer matching.
+        <div class="label">
+            <label for="field_auth">
+                Accept
+                <input type="checkbox" name="auth" id="field_auth" class="validate-required-check" title="This is required" />
+            </label>
+        </div>
+    </div>
+
+    <div>
+        By clicking "Create Account" the above organization claims responsibility to make the payment of the annual membership fee of $130.
+        <div class="label">
+            <label for="field_pay">
+                Accept
+                <input type="checkbox" name="pay" id="field_pay" class="validate-required-check" />
+            </label>
+        </div>
     </div>
     
     <div>
