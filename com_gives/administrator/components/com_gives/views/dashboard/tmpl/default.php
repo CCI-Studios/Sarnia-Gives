1592a778bfe5cf0579a6b0a7364c50fd10c9d761
Jonathan Bennett <jbennett@ccistudios.com>
2012-10-31 15:47:48 -0400

    Download reports of orgs and cols

--- com_gives/administrator/components/com_gives/views/dashboard/tmpl/default.php
+++ com_gives/administrator/components/com_gives/views/dashboard/tmpl/default.php
@@ -1,1 +1,16 @@
-dashboard
+<p>Downloads:<br/><em>note, right click and select save as</em></p>
+
+<ul>
+	<li><a href="<?= @route('format=csv&view=volunteers&limit=100000') ?>">
+		Volunteers
+	</a></li>
+	<li><a href="<?= @route('format=csv&view=organizations&limit=10000') ?>">
+		All Orgnaizations
+	</a></li>
+	<li><a href="<?= @route('format=csv&enabled=1&view=organizations&limit=10000') ?>">
+		Active Organizations
+	</a></li>
+	<li><a href="<?= @route('format=csv&enabled=0&view=organizations&limit=10000') ?>">
+		Expired Organizations
+	</a></li>
+</ul>
